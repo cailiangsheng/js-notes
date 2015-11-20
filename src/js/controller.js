ead6c46bf43d38256ea97da87e8b54a6134153f9
cailiangsheng <cailiangsheng@me.com>
2015-11-21 02:36:17 +0800

    display timestamp for short

--- src/js/controller.js
+++ src/js/controller.js
@@ -1,23 +1,10 @@
 var notesApp = angular.module('notesApp', []);
 var KEY_CODE_ENTER = 13;
 
-notesApp.filter('toTextLines', ['$sce', function ($sce) {
-    return function (text) {
-        if (!text) return text;
-
-        var lines = text.split("\n").join("<br>");
-        return $sce.trustAsHtml(lines);
-    }
-}]);
-
 notesApp.controller('initNotes', ['$scope', function ($scope) {
     var persistence = new PersistedNotes();
     $scope.title = "Larry's Notes";
     $scope.notes = persistence.notes.findNotes();
-
-    $scope.getDateTime = function (note) {
-        return new Date(note.timestamp).toLocaleString();
-    };
 
     $scope.fetchNotes = function () {
         $scope.notes = persistence.notes.findNotes($scope.inputText);
---
+++ src/js/filter.js
@@ -0,0 +1,20 @@
+
+notesApp.filter('toTextLines', ['$sce', function ($sce) {
+    return function (text) {
+        if (!text) return text;
+
+        var lines = text.split("\n").join("<br>");
+        return $sce.trustAsHtml(lines);
+    }
+}]);
+
+notesApp.filter('toDateTime', ['$sce', function ($sce) {
+    return function (timestamp) {
+        var today = new Date().toLocaleDateString();
+        var day = new Date(timestamp);
+        var date = day.toLocaleDateString();
+        var time = day.toLocaleTimeString();
+        var result = (today == date ? time : date);
+        return $sce.trustAsHtml(result);
+    }
+}]);
