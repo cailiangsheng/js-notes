652878f67637acf502c2b2ca9a2efcecd86c54c8
cailiangsheng <cailiangsheng@me.com>
2015-11-29 23:28:38 +0800

    more tests

--- test/note.spec.js
+++ test/note.spec.js
@@ -1,5 +1,3 @@
-var expect = chai.expect;
-
 describe("Note model", function () {
     it("has default values", function () {
         var note = new Note();
@@ -16,4 +14,20 @@ describe("Note model", function () {
         expect(note.title).to.equal("ABC");
         expect(note.content).to.equal("abc");
     });
+
+    it("can be updated with other note", function () {
+        var note1 = new Note("ABC", "abc");
+        var note2 = new Note("XYZ", "xyz");
+        note1.update(note2);
+
+        expect(note1.title).to.equal("XYZ");
+        expect(note2.content).to.equal("xyz");
+    })
+
+    it("can be matched with a keyword", function () {
+        var note = new Note("ABC", "abc");
+
+        expect(note.match("c")).to.be.true;
+        expect(note.match("x")).to.be.false;
+    })
 });
