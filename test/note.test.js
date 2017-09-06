0ed8dd398b56b0d1d611dc4a0d3db3d6f6ef83cd
Larry Cai <larry.cai@sigma.namee>
2017-09-06 21:12:13 +0800

    fix unit tests

--- test/note.test.js
+++ test/note.test.js
@@ -1,36 +1,36 @@
 var Note = require('../src/model/note');
 var expect = require('chai').expect;
 
-describe("Note model", function () {
-    it("has default values", function () {
+describe('model.Note', () => {
+    it('can be created with default values', () => {
         var note = new Note();
 
         expect(note).to.be.ok;
-        expect(note.title).to.equal("");
-        expect(note.content).to.equal("");
-        expect(note.timestamp).to.be.a("Number");
+        expect(note.title).to.equal('');
+        expect(note.content).to.equal('');
+        expect(note.timestamp).to.be.a('Number');
     });
 
-    it("sets passed attributes", function () {
-        var note = new Note("ABC", "abc");
+    it('can be created with specified values', () => {
+        var note = new Note('ABC', 'abc');
 
-        expect(note.title).to.equal("ABC");
-        expect(note.content).to.equal("abc");
+        expect(note.title).to.equal('ABC');
+        expect(note.content).to.equal('abc');
     });
 
-    it("can be updated with other note", function () {
-        var note1 = new Note("ABC", "abc");
-        var note2 = new Note("XYZ", "xyz");
+    it('can be updated with other note instance', () => {
+        var note1 = new Note('ABC', 'abc');
+        var note2 = new Note('XYZ', 'xyz');
         note1.update(note2);
 
-        expect(note1.title).to.equal("XYZ");
-        expect(note2.content).to.equal("xyz");
+        expect(note1.title).to.equal('XYZ');
+        expect(note2.content).to.equal('xyz');
     });
 
-    it("can be matched with a keyword", function () {
-        var note = new Note("ABC", "abc");
+    it('can be matched with a keyword', () => {
+        var note = new Note('ABC', 'abc');
 
-        expect(note.match("c")).to.be.true;
-        expect(note.match("x")).to.be.false;
+        expect(note.match('c')).to.be.true;
+        expect(note.match('x')).to.be.false;
     });
 });
--- test/notes.test.js
+++ test/notes.test.js
@@ -1,29 +1,35 @@
 var Notes = require('../src/model/notes');
 var expect = require('chai').expect;
 
-describe("Notes model", function () {
-    var notes = new Notes();
+describe('model.Notes', () => {
 
-    it("is an empty array by default", function () {
-        expect(notes.items).to.be.a("Array");
+    it('can be created with empty array by default', () => {
+        // when
+        var notes = new Notes();
+
+        // then
+        expect(notes.items).to.be.a('Array');
         expect(notes.items.length).to.equal(0);
     })
 
-    it("can be initialized with an array", function () {
+    it('can be initialized with an array and cleared', () => {
+        // when
+        var notes = new Notes();
         var array = [
-            {title: "ABC", content: "abc", timestamp: 123},
-            {title: "XYZ", content: "xyz", timestamp: 789}
+            {title: 'ABC', content: 'abc', timestamp: 123},
+            {title: 'XYZ', content: 'xyz', timestamp: 789}
         ];
         notes.initNotes(array);
 
+        // then
         expect(notes.items.length).to.equal(2);
-        expect(notes.items[0].title).to.equal("ABC");
-        expect(notes.items[1].content).to.equal("xyz");
-    });
+        expect(notes.items[0].title).to.equal('ABC');
+        expect(notes.items[1].content).to.equal('xyz');
 
-    it("can be cleared", function () {
+        // when
         notes.clearNotes();
 
+        // then
         expect(notes.items.length).to.equal(0);
     });
 });
